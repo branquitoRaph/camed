1e21bf827c532494219dd6628eddffdbf3f2b244
20201TIIMI0101 <jonathancastrosilva8@gmail.com>
2022-10-14 10:25:02 -0300

    Telas terminadas, ver detalhes

---
+++ app/src/main/java/com/jojora/camed/MudarPerfilActivity.java
@@ -0,0 +1,14 @@
+package com.jojora.camed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MudarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mudar_perfil);
+    }
+}
