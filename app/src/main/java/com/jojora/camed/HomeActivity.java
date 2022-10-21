7a0f6178e3ac513054eb9e26f67e14d24f521651
20201TIIMI0101 <jonathancastrosilva8@gmail.com>
2022-10-21 10:23:52 -0300

    Mudar o resto do mudado AaAAaaAAAAaa

--- app/src/main/java/com/jojora/camed/HomeActivity.java
+++ app/src/main/java/com/jojora/camed/HomeActivity.java
@@ -1,38 +1,30 @@
 package com.jojora.camed;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ImageButton botaoimage;
-    private ImageButton botaoimagelista1;
-    private ImageButton botaoimagelista2;
-    private ImageButton botaoimagelista3;
-    private ImageButton botaoimagelista4;
-    private ImageButton botaoimagelista5;
-    private ImageButton botaoimagelista6;
-    private Button botaocontato;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        botaoimage = findViewById(R.id.imageButtonHomeIrPerfil);
-        botaoimagelista1 = findViewById(R.id.imageButtonHomeSintoma1);
-        botaoimagelista2 = findViewById(R.id.imageButtonHomeSintoma2);
-        botaoimagelista3 = findViewById(R.id.imageButtonHomeSintoma3);
-        botaoimagelista4 = findViewById(R.id.imageButtonHomeSintoma4);
-        botaoimagelista5 = findViewById(R.id.imageButtonHomeSintoma5);
-        botaoimagelista6 = findViewById(R.id.imageButtonHomeSintoma6);
-        botaocontato = findViewById(R.id.buttonHomeContato);
-
+        Toolbar toolbar = findViewById(R.id.toolbarHome);
+        setSupportActionBar(toolbar);
+        /*
         botaoimage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +33,7 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
 
         botaoimagelista1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,5 +97,34 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        botaosobre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(HomeActivity.this, SobreActivity.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_activity_tb, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opPerfil:
+                Intent intent = new Intent(HomeActivity.this, PerfilActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
